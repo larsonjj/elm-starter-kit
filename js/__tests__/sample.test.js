18a9cd106c5b3ccaefdb7593e7fdbbd11b037f6b
Jake Larson <jake.j.larson@gmail.com>
2016-08-19 01:15:16 -0500

    removed console.log

--- js/__tests__/sample.test.js
+++ js/__tests__/sample.test.js
@@ -1,12 +1,7 @@
 import test from 'ava';
 import {sum, product} from '../sample'
 
-test('sample.js: should pass the framework setup tests', (t) => {
-  t.is(2 + 2, 4)
-})
-
 test('sample.js: sum - should add 2 numbers', (t) => {
-  console.log(sum)
   t.is(sum(3, 5), 8);
 })
 
